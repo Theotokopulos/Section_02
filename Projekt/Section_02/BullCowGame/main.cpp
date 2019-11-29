6617510a53cb91a7b7a347d2826f8a002f66f1c4
Adaś <torrac99912@gmail.com>
2019-11-29 17:31:03 +0100

    Sekcja2 c++

---
+++ Projekt/Section_02/BullCowGame/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()	
+{
+	//introduce the game
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls ad Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout <<" letter isogram I'm thinking of?\n";
+	cout << endl;
+
+	//get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline (std::cin, Guess);
+	//repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+	cout << endl;
+
+	//get a guess from the player
+	cout << "Enter your guess: ";
+	getline(std::cin, Guess);
+	//repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+	cout << endl;
+
+	return 0;
+}
