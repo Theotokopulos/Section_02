fed5fc99f9eb8edaced53bad2587fdc94aed1adc
Adaś <torrac99912@gmail.com>
2019-12-14 19:44:32 +0100

    Dodano "struct BullCowCount" i parę mniejszych rzeczy do tego

--- Projekt/Section_02/BullCowGame/FBullCowGame.cpp
+++ Projekt/Section_02/BullCowGame/FBullCowGame.cpp
@@ -15,6 +15,8 @@ void FBullCowGame::Reset()
 	constexpr int32 MAX_TRIES = 8;
 	MyMaxTries = MAX_TRIES ;
 
+	const Fstring HIDDEN_WORD = "planet";
+	MyHiddenWord = HIDDEN_WORD;
 	MyCurrentTry = 1;
 	
 	return;
@@ -28,4 +30,17 @@ bool FBullCowGame::IsGameWon()const
 bool FBullCowGame::CheckGuessValidity(Fstring)
 {
 	return false;
+}
+//receives a valid guess, increments turn and returns count
+BullCowCount FBullCowGame::SubmitGuess(Fstring)
+{
+	//increment the turn number
+	MyCurrentTry++;
+
+	//setup a return variable
+	BullCowCount BullCowCount;
+
+	//loop through all letters in the guess
+		//compare against the hidden word
+	return BullCowCount;
 }
--- Projekt/Section_02/BullCowGame/FBullCowGame.h
+++ Projekt/Section_02/BullCowGame/FBullCowGame.h
@@ -2,8 +2,13 @@
 #include <string>
 
 using int32 = int;
-
 using Fstring = std::string;
+
+struct BullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
 
 class FBullCowGame 
 {
@@ -17,7 +22,11 @@ public:
 	void Reset();
 	bool CheckGuessValidity(Fstring);
 
+	//Counts bulls and cows and increases try # assuming valid guess
+	BullCowCount SubmitGuess(Fstring);
+
 private: //inicjalizacja w construktorze
 	int32 MyCurrentTry;
 	int32 MyMaxTries;
+	Fstring MyHiddenWord;
 };
